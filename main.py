e632cc87ea81d14783bcfce543f8238bc12691ba
Shantanu Tripathi <40469481+Daishinkan002@users.noreply.github.com>
2020-08-28 23:03:39 +0530

    Init File

---
+++ main.py
@@ -0,0 +1,15 @@
+import matplotlib
+matplotlib.use('Agg')
+from tkinter import Tk
+from pdfviewer import PDFViewer
+
+  
+
+
+def main():
+    root = Tk()
+    PDFViewer()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
